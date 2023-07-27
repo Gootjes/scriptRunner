804f725f1e6bff3df0c4a112643a9662d8bb95de
Gootjes <Gootjes@users.noreply.github.com>
2023-07-28 00:01:25 +0200

    changed detect_scripts argument to be include_status instead of exclude

--- R/detect_scripts.R
+++ R/detect_scripts.R
@@ -1,16 +1,23 @@
 
 #' @title Detect scripts in a folder
+#' @param base_path Path to start search from
+#' @param include_status Character vector describing which status to filter on.
+#'  Use \code{NULL} to include all
+#' @param recursive Whether to look recursively in folders
 #' @importFrom glue glue
 #' @export
-detect_scripts <- function(base_path, exclude_status = c("LOCKED"), recursive = TRUE) {
+detect_scripts <- function(base_path, include_status = c("NOT_STARTED",
+                                                         "LOCKED",
+                                                         "HALTED",
+                                                         "COMPLETED"), recursive = TRUE) {
 
   dirs <- c()
 
   for(d in list.dirs(path = base_path, recursive = recursive)) {
     if (file.exists(glue("{d}/{opt('marker_filename')}"))) {
-      if(length(exclude_status) > 0) {
-        s <- get_script_status(d, all = TRUE)[exclude_status]
-        if(!any(s, na.rm = T)) {
+      if(length(include_status) > 0) {
+        s <- get_script_status(d, all = TRUE)[include_status]
+        if(any(s, na.rm = T)) {
           dirs <- c(dirs, d)
         }
       } else {
