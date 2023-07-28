3059613ce6cd0b9844c25e75a032a4860aed436f
Gootjes <Gootjes@users.noreply.github.com>
2023-07-28 15:10:18 +0200

    improved arguments to run_script with better defaults

--- R/run_script.R
+++ R/run_script.R
@@ -2,13 +2,25 @@
 
 #' @title Run script made with \code{\link{make_script}}
 #' @param script_path Path to the folder of the script
-#' @param wait Whether to wait for the script to complete (Default) or not
+#' @param rscript_binary Location of rscript binary
+#' @param wait see ?system
+#' @param ignore.stdout see ?system
+#' @param ignore.stderr see ?system
+#' @param show.output.on.console see ?system
+#' @param timeout see ?system
 #' @examples
 #' run_script(make_script({ ... }, name = 'test-1', folder = 'test-scripts'))
 #'
 #' @importFrom glue glue
 #' @export
-run_script <- function(script_path, wait = TRUE, rscript = NULL) {
+run_script <- function(script_path,
+                       rscript_binary = file.path(R.home("bin"), 'Rscript'),
+                       wait = TRUE,
+                       ignore.stdout = TRUE,
+                       ignore.stderr = TRUE,
+                       show.output.on.console = FALSE,
+                       timeout = 0,
+                       ...) {
 
   with_script(script_path, {
 
@@ -20,11 +32,14 @@ run_script <- function(script_path, wait = TRUE, rscript = NULL) {
       stop("No script to run found!")
     }
 
-    if(is.null(rscript)) {
-      rscript <- file.path(R.home("bin"), 'Rscript')
-    }
-
-    ret <- system(glue("{rscript} --restore --save {opt('script_filename')}"), wait = wait)
+    ret <- system(command = glue("{rscript_binary} --restore --save {opt('script_filename')}"),
+                  intern = FALSE,
+                  wait = wait,
+                  ignore.stdout = TRUE,
+                  ignore.stderr = TRUE,
+                  show.output.on.console = FALSE,
+                  timeout = 0,
+                  ...)
 
     if(ret != 0) {
       warning("There was an error during execution", call. = F, immediate. = T)
